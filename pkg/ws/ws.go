3723f7dec671c98df00355b91ae1751b63a66b3c
jc <46619361+juancwu@users.noreply.github.com>
2023-12-01 14:52:22 -0500

    add simple send/receive mechanism

--- pkg/ws/ws.go
+++ pkg/ws/ws.go
@@ -2,6 +2,8 @@ package ws
 
 import (
 	"fmt"
+	"time"
+    "encoding/json"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
@@ -11,23 +13,87 @@ var (
     upgrader = websocket.Upgrader{}
 )
 
+type Message struct {
+    ChatMessage string `json:"chat_message"`
+}
+
+type Client struct {
+    Conn *websocket.Conn
+    MsgChan chan string
+}
+
+func (c *Client) readMessage(ctx echo.Context) {
+    err := c.Conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+    if err != nil {
+        ctx.Logger().Error(err)
+        return
+    }
+
+    c.Conn.SetPongHandler(func (appData string) error {
+        err := c.Conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+        if err != nil {
+            ctx.Logger().Error(err)
+            return err
+        }
+        fmt.Println("pong")
+        return nil
+    })
+
+    defer c.Conn.Close()
+    
+    for {
+        _, msg, err := c.Conn.ReadMessage()
+        if err != nil {
+            return
+        }
+        var message Message
+        err = json.Unmarshal(msg, &message)
+        if err != nil {
+            ctx.Logger().Error(err)
+            continue
+        }
+        fmt.Printf("From Client: %s\n", message.ChatMessage)
+        c.MsgChan <- message.ChatMessage
+    }
+}
+
+func (c *Client) writeMessage(ctx echo.Context) {
+    defer c.Conn.Close()
+
+    ticker := time.NewTicker(time.Second * 9)
+    for {
+        select {
+        case text, ok := <-c.MsgChan:
+            if !ok {
+                return
+            }
+            fmt.Printf("Message to send: %s\n", text)
+            err := c.Conn.WriteMessage(websocket.TextMessage, []byte(text))
+            if err != nil{
+                ctx.Logger().Error(err)
+                return
+            }
+        case <- ticker.C:
+            err := c.Conn.WriteMessage(websocket.PingMessage, []byte(""))
+            if err != nil {
+                ctx.Logger().Error(err)
+                return
+            }
+            fmt.Println("Ping")
+    }
+    }
+}
+
 func Handle (c echo.Context) error {
     ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
     if err != nil {
         return err
     }
-    defer ws.Close()
 
-    for {
-        err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
-        if err != nil {
-            c.Logger().Error(err)
-        }
+    client := Client{ws, make(chan string)}
 
-        _, msg, err := ws.ReadMessage()
-        if err != nil {
-            c.Logger().Error(err)
-        }
-        fmt.Printf("%s\n", msg)
-    }
+    go client.readMessage(c)
+    go client.writeMessage(c)
+
+    return nil
 }
